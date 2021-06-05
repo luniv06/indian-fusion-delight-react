a13bb5d932afc1923552c3b950215f6a1096be2f
Vinul James <vinuljames@gmail.com>
2021-06-05 20:26:04 +0530

    Configuring react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar dark color="primary">
+        <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
     </div>
   );
 }
